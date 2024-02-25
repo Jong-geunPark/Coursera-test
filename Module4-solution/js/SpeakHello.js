c4a7fa5c790a81bcac2e2a70b5bfaf08d45ef066
Jong-Geun <152132036+Jong-geunPark@users.noreply.github.com>
2024-02-25 08:57:54 -0300

    Update SpeakHello.js

--- Module4-solution/js/SpeakHello.js
+++ Module4-solution/js/SpeakHello.js
@@ -1,1 +1,15 @@
+// STEP 2
+(function (window) {
+  
+// STEP 3
+    var helloSpeaker = {};
+    var speakWord = "Hello";
 
+// STEP 4   
+    helloSpeaker.speak = function (names) {
+      console.log(speakWord + " " + names);
+    }
+  
+// STEP 5
+        window.helloSpeaker = helloSpeaker;
+    })(window);
